77437b642735223abc9ed25c08ae0fdfb9467154
Robert Rees <shudderfix@gmail.com>
2019-02-09 23:59:19 +0000

    Validates the log data

--- app/handlers/forms.py
+++ app/handlers/forms.py
@@ -3,7 +3,9 @@ import flask
 from app import forms
 
 def log():
-
+	log_form = forms.LogForm(flask.request.form)
+	if log_form.validate():
+		pass
 	return flask.redirect(flask.url_for('home'))
 
 
